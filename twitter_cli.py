2c5998728628dbd4d63cefcdebf106fa9fa9fbd2
Oleg Strizhechenko <oleg@carbonsoft.ru>
2014-12-30 13:00:55 +0500

    Update twitter_cli.py

--- twitter_cli.py
+++ twitter_cli.py
@@ -1,40 +1,18 @@
+#!/usr/bin/python
+
 # coding: utf-8
 # author: oleg
-import tweepy, threading, sys
-from time import localtime, strftime, sleep
+import sys
+import tweepy
 
-replyed = ['']
-update_time = 60
-
-def init():
-    global api
-    consumer_key = ""
-    consumer_secret = ""
-    access_token = ""
-    access_token_secret = ""
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
-    api = tweepy.API(auth)
-
-
-class TwiBot(threading.Thread):
-    def __init__(self):
-        threading.Thread.__init__(self)
-
-    def tweet(self, twit):
-        if len(twit) <= 140 and len(twit) > 0:
-            try:
-                api.update_status(twit)
-                return True
-            except tweepy.TweepError:
-                print 'Duplicate'
-        return False
-
-line = ''
+msg = ''
 sys.argv.remove(sys.argv[0])
 for i in sys.argv:
-    line = line + i + " "
-
-init()
-s = TwiBot()
-s.tweet(line)
+    msg = msg + i + " "
+consumer_key = ""
+consumer_secret = ""
+access_key = ""
+access_secret = ""
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_key, access_secret)
+tweepy.API(auth).update_status(msg)
