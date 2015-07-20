871fb0315a91039926ec3f73f27c2b44b5a2acb0
Oleg Strizhechenko <oleg@carbonsoft.ru>
2015-07-20 11:58:07 +0500

    Update twitter_cli.py

--- twitter_cli.py
+++ twitter_cli.py
@@ -2,13 +2,14 @@
 # coding: utf-8
 # author: oleg
 
-import sys, tweepy
+import sys
+from tweepy import OAuthHandler, API
 
 msg = reduce(lambda x, y: x + " " + y, sys.argv[1:])
 consumer_key = ""
 consumer_secret = ""
 access_key = ""
 access_secret = ""
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_key, access_secret)
-tweepy.API(auth).update_status(msg)
+API(auth).update_status(msg)
